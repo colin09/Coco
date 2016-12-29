a404ca4cf66212e2cf0198d8f3a87a4624b4354b
colin.du <337810174@qq.com>
2016-12-29 18:36:11 +0800

    package controller

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,5 +1,58 @@
 console.log("vs code start....");
-
 console.log(typeof(window));
-
 console.log(global.process.cwd());
+
+
+
+const Koa = require('koa'); // 导入koa，和koa 1.x不同，在koa2中，我们导入的是一个class，因此用大写的Koa表示:
+//const router = require('koa-router')(); // 注意require('koa-router')返回的是函数:
+const bodyParser = require('koa-bodyparser');
+const controller = require('./controller'); //导入controller middleware
+
+// 创建一个Koa对象表示web app本身:
+const app = new Koa();
+
+/*
+// 对于任何请求，app将调用该异步函数处理请求：
+app.use(async (ctx, next) => {
+    //console.log(ctx);
+    const start = new Date().getTime(); // 当前时间
+    //console.log(`${ctx.request.method} ${ctx.request.url}`); // 打印URL
+    console.log(`${ctx.method} ${ctx.url}`); // 打印URL 同上
+
+    await next();
+
+    const ms = new Date().getTime() - start; // 耗费时间
+    console.log(`Time: ${ms}ms`); // 打印耗费时间
+
+    ctx.response.type = 'text/html';
+    ctx.response.body = '<h1>Hello, nodejs / Koa2 / ES7!</h1>';
+});
+
+// add url-route:
+router.get('/list', async (ctx, next) => {
+    console.log(`params: ${ctx.params}`);
+    ctx.response.body = `<h1>Hello, list !</h1>`;
+});
+
+router.get('/', async (ctx, next) => {
+    ctx.response.body = '<h1>Index</h1>';
+});
+*/
+
+
+//由于middleware的顺序很重要，这个koa-bodyparser必须在router之前被注册到app对象上
+app.use(bodyParser());
+
+// 使用middleware:
+app.use(controller());
+
+// add router middleware:
+//app.use(router.routes());
+
+
+
+
+// 在端口3000监听:
+app.listen(3000);
+console.log('app started at port 3000...');
---
+++ nodejs/controllers/users.js
@@ -0,0 +1,35 @@
+var fn_index = async (ctx, next) => {
+    ctx.response.body = `<h1>Index</h1>
+        <form action="/signin" method="post">
+            <p>Name: <input name="name" value="koa"></p>
+            <p>Password: <input name="password" type="password"></p>
+            <p><input type="submit" value="Submit"></p>
+        </form>`;
+};
+
+
+var fn_signin = async (ctx, next) => {
+    var
+        name = ctx.request.body.name || '',
+        password = ctx.request.body.password || '';
+    console.log(`signin with name: ${name}, password: ${password}`);
+
+    if (name === 'mgr' && password === '12345') {
+        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
+    } else {
+        ctx.response.body = `<h1>Login failed!</h1>
+        <p><a href="/">Try again</a></p>`;
+    }
+};
+
+var fn_hello = async (ctx, next) => {
+    var name = ctx.params.name;
+    ctx.response.body = `<h1>Hello, ${name}!</h1>`;
+};
+
+
+module.exports={
+    'GET /':fn_index,
+    'POST /SignIn':fn_signin,
+    'GET /hello/:name': fn_hello
+}
