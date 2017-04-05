69535ad745db3e5b41c6432167d68565398a9188
colin.du <337810174@qq.com>
2017-04-05 11:27:12 +0800

    store

---
+++ golden/src/app/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ClassLibrary;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine($"The answer is {new Thing().Get(19, 23)}");
+        }
+    }
+}
---
+++ golden/test/test-library/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+using ClassLibrary;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(42, new Thing().Get(19, 23));
+        }
+    }
+}
