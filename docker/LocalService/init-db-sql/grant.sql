0914e8e1fef8838738ffc78e6a960dbb925c9ad6
duxinglong <duxinglong@yijiupi.cn>
2018-06-25 08:48:26 +0800

    docker - mysql sql

--- docker/LocalService/init-db-sql/grant.sql
+++ docker/LocalService/init-db-sql/grant.sql
@@ -1,4 +1,4 @@
 -- grant privileges
 -- mysql -u root -p
--- grant all privileges on *.* to root@'%' identified by 'root-1234';
+ grant all privileges on *.* to root@'%' identified by 'root-1234';
 
