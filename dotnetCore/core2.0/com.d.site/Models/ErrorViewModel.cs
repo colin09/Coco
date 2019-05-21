fd67f59f48e65c671e9bd8690cc78d5a00923035
dxl <duxinglong@yijiupi.cn>
2019-05-21 10:49:34 +0800

    dockerfile

---
+++ dotnetCore/core2.0/com.d.site/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace com.d.site.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- dotnetCore/jiu/jiu.CreateDS/output/service/PurchaseTaskModule/IPurchaseTaskQueryService.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-namespace LDTech.ERP.Domain.BusinessServices.PurchaseTaskModule {
-public interface IPurchaseTaskQueryService {
-}
-}
